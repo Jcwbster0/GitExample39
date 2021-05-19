e8a902ac67dbac9de6ba5722dbd5b89ca6245fbe
Justin Webster <justinwebster@Justins-MBP.localdomain>
2021-05-19 10:10:48 -0600

    setup initial project file

---
+++ GitPRactie/GitPRactie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPRactie
+//
+//  Created by Justin Webster on 5/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
